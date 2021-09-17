a427015095055fdfe7345f5ee485a926282f9c2c
Mahlangu <34237978+PTMahlangu@users.noreply.github.com>
2021-09-17 23:10:37 +0200

    Update MapBoxAutoCompleteWidget.dart

--- lib/src/MapBoxAutoCompleteWidget.dart
+++ lib/src/MapBoxAutoCompleteWidget.dart
@@ -73,7 +73,7 @@ class _MapBoxAutoCompleteWidgetState extends State<MapBoxAutoCompleteWidget> {
 
       setState(() {
         _placePredictions = predictions;
-         print(_placePredictions.features);
+         print(_placePredictions.features.toString());
       });
     } else {
       setState(() => _placePredictions = Predections.empty());
